fe98c3c663ba4c9ace847a93b9c7d9d4dbe04681
Valeriya Pavlova <vla.valeriya@gmail.com>
2023-09-28 20:51:13 +0300

    update

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -5,6 +5,7 @@ import { userSchemas } from "../../models/User.js";
 import validateBody from "../../middleware/validateBody.js";
 import AuthControllers from "../../controllers/auth-controllers.js";
 import authenticate from "../../middleware/authenticate.js";
+import upload from "../../middleware/upload.js";
 
 const authRouter = express.Router();
 
@@ -16,4 +17,11 @@ authRouter.post("/users/register", userSignupValidate, AuthControllers.signup);
 authRouter.post("/users/login", userSigninValidate, AuthControllers.signin);
 authRouter.get("/current", authenticate, AuthControllers.getCurrent);
 authRouter.post("/users/signout", authenticate, AuthControllers.signout);
+authRouter.patch(
+  "/users/avatars",
+  upload.single("avatar"),
+  authenticate,
+  AuthControllers.updateAvatar
+);
+
 export default authRouter;
