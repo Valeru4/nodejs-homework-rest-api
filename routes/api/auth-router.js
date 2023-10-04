375655a4249dfbe74f761dff3728c8a7cfc2d768
Valeriya Pavlova <vla.valeriya@gmail.com>
2023-10-04 18:31:50 +0300

    update

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -12,11 +12,18 @@ const authRouter = express.Router();
 const userSignupValidate = validateBody(userSchemas.signUpSchema);
 
 const userSigninValidate = validateBody(userSchemas.signInSchema);
+const userEmailValidate = validateBody(userSchemas.userEmailSchema);
 
 authRouter.post("/users/register", userSignupValidate, AuthControllers.signup);
 authRouter.post("/users/login", userSigninValidate, AuthControllers.signin);
 authRouter.get("/current", authenticate, AuthControllers.getCurrent);
 authRouter.post("/users/signout", authenticate, AuthControllers.signout);
+authRouter.get("/users/verify/:verificationToken", authControler.verify);
+authRouter.post(
+  "/users/verify",
+  userEmailValidate,
+  authControler.resendVerifyEmail
+);
 authRouter.patch(
   "/users/avatars",
   upload.single("avatar"),
